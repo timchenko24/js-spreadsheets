9c09779846f6cf638f5ff48cced54702086fa39a
Tim Chenskiy <timadraft@gmail.com>
2020-10-27 22:02:39 +0300

    input and keydown events

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,24 +1,49 @@
 import {SpreadsheetComponent} from '@core/SpreadsheetComponent';
+import {$} from '@core/DOM';
 
 export class Formula extends SpreadsheetComponent {
   static className = 'main__formula'
 
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
       name: 'Formula component',
-      listeners: ['input'],
+      listeners: ['input', 'keydown'],
+      ...options,
     });
   }
 
   toHTML() {
     return `
       <div class="info">fx</div>
-      <div class="input" contenteditable spellcheck="false"></div>
+      <div id="formula" class="input" contenteditable spellcheck="false"></div>
     `;
   }
 
+  add() {
+    super.add();
+
+    this.$formula = this.$root.find('#formula');
+
+    this.$on('table:select', ($cell) => {
+      this.$formula.text($cell.text());
+    });
+
+    this.$on('table:input', ($cell) => {
+      this.$formula.text($cell.text());
+    });
+  }
+
   onInput(event) {
-    console.log(this.$root);
-    console.log('f on input', event.target.textContent.trim());
+    this.$emit('formula:input', $(event.target).text());
+  }
+
+  onKeydown(event) {
+    const {key} = event;
+    const keys = ['Enter', 'Tab'];
+
+    if (keys.includes(key)) {
+      event.preventDefault();
+      this.$emit('formula:complete');
+    }
   }
 }
