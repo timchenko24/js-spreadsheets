d82fe14948786d8da384fd3eb7b883cb85d24a5d
Tim Chenskiy <timadraft@gmail.com>
2020-10-27 22:01:23 +0300

    emit and on methods

--- src/core/SpreadsheetComponent.js
+++ src/core/SpreadsheetComponent.js
@@ -4,6 +4,8 @@ export class SpreadsheetComponent extends DOMListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
     this.name = options.name || '';
+    this.emitter = options.emitter;
+    this.unsubscribers = [];
 
     this.prepare();
   }
@@ -16,11 +18,21 @@ export class SpreadsheetComponent extends DOMListener {
     return '';
   }
 
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args);
+  }
+
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn);
+    this.unsubscribers.push(unsub);
+  }
+
   add() {
     this.addDOMListeners();
   }
 
   destroy() {
     this.removeDOMListener();
+    this.unsubscribers.forEach((unsub) => unsub());
   }
 }
