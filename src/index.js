bb811dcbe8f5a33de648a4c3810efd4596d6fd6d
Tim Chenskiy <timadraft@gmail.com>
2020-10-28 15:08:53 +0300

    store base logic

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import {Header} from '@/components/header/Header';
 import {Toolbar} from '@/components/toolbar/Toolbar';
 import {Formula} from '@/components/formula/Formula';
 import {Table} from '@/components/table/Table';
+import {createStore} from '@core/createStore';
+import {rootReducer} from '@/store/rootReducer';
+
+const store = createStore(rootReducer());
 
 const spreadsheet = new Spreadsheet('#app', {
   components: [Header, Toolbar, Formula, Table],
+  store,
 });
 
 spreadsheet.render();
