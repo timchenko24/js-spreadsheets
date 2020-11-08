5a95c19c3b6dd5d7b429613e0b7a83ff66e4eead
Tim Chenskiy <timadraft@gmail.com>
2020-11-08 18:02:09 +0300

    router

---
+++ src/core/routes/Router.js
@@ -0,0 +1,42 @@
+import {$} from '@core/DOM';
+import {ActiveRoute} from '@core/routes/ActiveRoute';
+
+export class Router {
+  constructor(selector, routes) {
+    if (!selector) {
+      throw new Error('Selector is not provided in Router');
+    }
+
+    this.$placeholder = $(selector);
+    this.routes = routes;
+    this.page = null;
+
+    this.changePageHandler = this.changePageHandler.bind(this);
+
+    this.init();
+  }
+
+  init() {
+    window.addEventListener('hashchange', this.changePageHandler);
+  }
+
+  destroy() {
+    window.removeEventListener('hashchange', this.changePageHandler);
+  }
+
+  changePageHandler() {
+    if (this.page) {
+      this.page.destroy();
+    }
+    this.$placeholder.clear();
+
+    const Page = ActiveRoute.path.includes('spreadsheet') ?
+      this.routes.spreadsheet :
+      this.routes.dashboard;
+    this.page = new Page(ActiveRoute.param);
+
+    this.$placeholder.append(this.page.getRoot());
+
+    this.page.afterRender();
+  }
+}
