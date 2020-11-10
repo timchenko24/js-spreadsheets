b303f3f727323759b66b5efb7afd1deacb66cd94
Tim Chenskiy <timadraft@gmail.com>
2020-11-10 18:17:02 +0300

    spreadsheet page own state

--- src/pages/SpreadsheetPage.js
+++ src/pages/SpreadsheetPage.js
@@ -1,7 +1,7 @@
 import {Page} from '@core/Page';
 import {createStore} from '@core/createStore';
 import {rootReducer} from '@/store/rootReducer';
-import {initialState} from '@/store/initialState';
+import {normalizeInitialState} from '@/store/initialState';
 import {debounce, storage} from '@core/utils';
 import {Spreadsheet} from '@/components/spreadsheet/Spreadsheet';
 import {Header} from '@/components/header/Header';
@@ -9,13 +9,19 @@ import {Toolbar} from '@/components/toolbar/Toolbar';
 import {Formula} from '@/components/formula/Formula';
 import {Table} from '@/components/table/Table';
 
+function storageName(param) {
+  return 'spreadsheet:' + param;
+}
+
 export class SpreadsheetPage extends Page {
   getRoot() {
-    const store = createStore(rootReducer, initialState);
+    const params = this.params ? this.params : Date.now().toString();
+    const state = storage(storageName(params));
+    const store = createStore(rootReducer, normalizeInitialState(state));
 
     const stateListener = debounce((state) => {
       console.log(state);
-      storage('main-state', state);
+      storage(storageName(params), state);
     }, 300);
 
     store.subscribe(stateListener);
