0e19f1d7429ed43edf56dba0816392bd6a051422
Tim Chenskiy <timadraft@gmail.com>
2020-11-10 18:32:25 +0300

    remove table and exit buttons

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,6 +3,7 @@ import {changeTitle} from '@/store/actions';
 import {$} from '@core/DOM';
 import {defaultTitle} from '@/constants';
 import {debounce} from '@core/utils';
+import {ActiveRoute} from '@core/routes/ActiveRoute';
 
 export class Header extends SpreadsheetComponent {
   static className = 'main__header'
@@ -10,7 +11,7 @@ export class Header extends SpreadsheetComponent {
   constructor($root, options) {
     super($root, {
       name: 'Header',
-      listeners: ['input'],
+      listeners: ['input', 'click'],
       ...options,
     });
   }
@@ -20,22 +21,37 @@ export class Header extends SpreadsheetComponent {
   }
 
   toHTML() {
-    const title = this.store.getState().title || defaultTitle;
+    const title = this.store.getState().tableName || defaultTitle;
     return `
       <input type="text" class="input" value="${title}" />
       <div>
-          <div class="button">
-                  <span class="material-icons">
-                      exit_to_app
-                  </span>
-          </div>
-          <div class="button">
-                  <span class="material-icons">
-                      delete
-                  </span>
-          </div>
+        <div class="button" data-button="exit">
+          <span class="material-icons" data-button="exit">
+              exit_to_app
+          </span>
+        </div>
+        <div class="button" data-button="remove">
+          <span class="material-icons" data-button="remove">
+              delete
+          </span>
+        </div>
       </div>
     `;
+  }
+
+  onClick(event) {
+    const $target = $(event.target);
+
+    if ($target.dataset.button === 'remove') {
+      const warning = confirm('Удалить таблицу?');
+
+      if (warning) {
+        localStorage.removeItem('spreadsheet:' + ActiveRoute.param);
+        ActiveRoute.navigate('');
+      }
+    } else if ($target.dataset.button === 'exit') {
+      ActiveRoute.navigate('');
+    }
   }
 
   onInput(event) {
