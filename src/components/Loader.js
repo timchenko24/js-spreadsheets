1f6c2d7bd0b8afd7f43754b47aa312c42d6ba34a
Tim Chenskiy <timadraft@gmail.com>
2020-11-12 15:51:18 +0300

    loader

---
+++ src/components/Loader.js
@@ -0,0 +1,20 @@
+import {$} from '../core/DOM';
+
+export function Loader() {
+  return $.create('div', 'loader').html(`
+    <div class="lds-default">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  `);
+}
